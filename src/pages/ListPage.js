0790c83094334cb5ada019375393ad90381cb40f
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-07-27 22:25:04 +0900

    🐛 원본데이터 오염방지를 위한 리팩토링 : store의 memoList 변경함수(검색하기, 정렬변경) ListPage로 옮기고 리팩토링

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,23 +1,117 @@
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux/es/hooks/useSelector";
+import styled from "styled-components";
 
 // constant
 import Colors from "../styles/Colors";
+import { FlexCenterBetween } from "../styles/Flex";
 
 // components
-import { Search } from "../components/Search";
 import { Button } from "../components/Button";
 import { List } from "../components/List";
+import { useState } from "react";
+
+const SearchStyle = styled.div`
+  ${FlexCenterBetween}
+  margin-bottom: 10px;
+`;
+const SearchInput = styled.input`
+  display: block;
+  width: calc(100% - 130px);
+  border: 1px solid ${Colors.border};
+  border-radius: 4px;
+  height: 40px;
+  padding: 0 10px;
+`;
+const SearchSelect = styled.select`
+  display: block;
+  width: 120px;
+  border: 1px solid ${Colors.border};
+  border-radius: 4px;
+  height: 40px;
+  padding: 0 10px;
+`;
 
 const ListPage = function () {
   const navigate = useNavigate();
-  const memoList = useSelector((state) => {
+
+  // store에서 memoList 가져오기 (원본데이터 오염방지)
+  const memoListOri = useSelector((state) => {
     return state.memoList;
   });
+  const [memoList, setMemoList] = useState([...memoListOri]);
+
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const selectOption = ["최신순", "등록순"];
+
+  // 검색하기
+  const getSearchResult = function (value) {
+    setSearchKeyword(value);
+
+    if (value !== "") {
+      let result = memoListOri.filter((item) => {
+        return (
+          item.title.toUpperCase().includes(value.toUpperCase()) ||
+          item.content.toUpperCase().includes(value.toUpperCase())
+        );
+      });
+      setMemoList(result);
+    } else {
+      setMemoList(memoListOri);
+    }
+  };
+
+  // 정렬하기
+  function sortMemoList(value) {
+    setSearchKeyword("");
+    value = parseFloat(value);
+    const copy = [...memoListOri];
+
+    // 정렬하기 - 최신순
+    if (value === 0) {
+      copy.sort((a, b) => {
+        return new Date(b.dateTime) - new Date(a.dateTime);
+      });
+      setMemoList(copy);
+    }
+
+    // 정렬하기 - 등록순
+    if (value === 1) {
+      copy.sort((a, b) => {
+        return new Date(a.dateTime) - new Date(b.dateTime);
+      });
+      setMemoList(copy);
+    }
+  }
 
   return (
     <div>
-      <Search></Search>
+      <SearchStyle>
+        <h2 className="visually-hidden">검색하기</h2>
+        <SearchInput
+          type="text"
+          aria-label="검색어를 입력해주세요"
+          placeholder="검색어를 입력해주세요"
+          onInput={(e) => {
+            getSearchResult(e.target.value);
+          }}
+          value={searchKeyword}
+        ></SearchInput>
+        <SearchSelect
+          aria-label="정렬변경"
+          onChange={(e) => {
+            sortMemoList(e.target.value);
+          }}
+        >
+          {selectOption.map((item, idx) => {
+            return (
+              <option key={idx} value={idx}>
+                {item}
+              </option>
+            );
+          })}
+        </SearchSelect>
+      </SearchStyle>
       <List memoList={memoList}></List>
       <Button
         $bg={Colors.blue}
--- src/store.js
+++ src/store.js
@@ -10,56 +10,10 @@ const memoList = createSlice({
     updateMemoList() {
       return JSON.parse(localStorage.getItem("memoList"));
     },
-
-    // 검색하기
-    getSearchResult(state, value) {
-      let copy =
-        JSON.parse(localStorage.getItem("memoList")) !== null
-          ? JSON.parse(localStorage.getItem("memoList"))
-          : [];
-      if (value !== "" && copy.length !== 0) {
-        let result = copy.filter((item) => {
-          return (
-            item.title.toUpperCase().includes(value.payload.toUpperCase()) ||
-            item.content.toUpperCase().includes(value.payload.toUpperCase())
-          );
-        });
-        return result;
-      } else {
-        return copy;
-      }
-    },
-
-    // 정렬하기
-    sortMemoList(state, sortOption) {
-      sortOption = parseFloat(sortOption.payload);
-
-      let copy =
-        JSON.parse(localStorage.getItem("memoList")) !== null
-          ? JSON.parse(localStorage.getItem("memoList"))
-          : [];
-
-      // 정렬하기 - 최신순
-      if (sortOption === 0) {
-        copy.sort((a, b) => {
-          return new Date(b.dateTime) - new Date(a.dateTime);
-        });
-        return copy;
-      }
-
-      // 정렬하기 - 등록순
-      if (sortOption === 1) {
-        copy.sort((a, b) => {
-          return new Date(a.dateTime) - new Date(b.dateTime);
-        });
-        return copy;
-      }
-    },
   },
 });
 
-export const { updateMemoList, getSearchResult, sortMemoList } =
-  memoList.actions;
+export const { updateMemoList } = memoList.actions;
 
 export default configureStore({
   reducer: {
